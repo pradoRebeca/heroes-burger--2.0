8ed7d028a6287a5bc9db4d3d23c9ec2f33ced885
rebeca.prado <rebeca.2003.prado@gmail.com>
2022-07-06 11:45:30 -0300

    feat: rota de delete/post/get do usuario

--- src/routes.ts
+++ src/routes.ts
@@ -3,8 +3,8 @@ import UserController from "./controllers/UserController";
 
 const routes = Router()
 
-routes.get('/user', UserController.index)
-routes.post('/user', UserController.create)
-
+routes.get('/v1/user', UserController.find)
+routes.post('/v1/user', UserController.create)
+routes.delete('/v1/user', UserController.delete)
 
 export default routes
