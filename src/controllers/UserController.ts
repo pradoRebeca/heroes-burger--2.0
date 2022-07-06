6edfb26ca2021ce76dae6b5c128af09b9d7e8600
rebeca.prado <rebeca.2003.prado@gmail.com>
2022-07-06 11:44:36 -0300

    feat: validação de criação  do usuário e CRUD

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,21 +1,54 @@
 import { Response, Request } from "express";
-import UserService from '../services/users/service'
+import UserService from "../services/users/service";
+import { buildErrorObject, existErrorObject } from "../utils/utils";
 
 class UserController {
-  public async index(req: Request, res: Response): Promise<Response> {
-    const users = await User.find();
-    return res.json(users);
+  public async find(req: Request, res: Response): Promise<Response> {
+    // const {name} = req.query
+    const name = "prado";
+    const responseService = await UserService.findUser(name);
+
+    if (!existErrorObject(responseService)) {
+      console.info(responseService);
+      return res.status(200).send(responseService);
+    }
+
+    console.error(responseService);
+    return res.status(400).send(responseService);
   }
 
   public async create(req: Request, res: Response): Promise<Response> {
-    const { email, firstName, lastName } = req.body;
+    const { name, password } = req.body;
 
-    if (email == "" || firstName == "" || lastName == "") {
-      return res.status(400).send({ message: "Vai dar para criar o user não" });
+    if (!name || !password) {
+      const errorResponse = buildErrorObject("Campos invalidos");
+      return res.status(400).send(errorResponse);
     }
 
-    UserService.create({ email, firstName, lastName });
-    return res.status(201).send({ data: user });
+    const responseService = await UserService.createUser({
+      name,
+      password,
+    });
+
+    if (!existErrorObject(responseService)) {
+      console.info(responseService);
+      return res.status(201).send(responseService);
+    }
+
+    console.error(responseService);
+    return res.status(400).send(responseService);
+  }
+
+  public async delete(req: Request, res: Response): Promise<Response> {
+    const { name } = req.body;
+
+    const responseService = await UserService.deleteUser(name);
+
+    if (!existErrorObject(responseService)) {
+      return res.status(200).send(responseService);
+    }
+
+    return res.status(200).send(responseService);
   }
 }
 
--- src/services/users/service.ts
+++ src/services/users/service.ts
@@ -1,11 +1,30 @@
-import User from '../../schemas/User'
+import User from "../../schemas/User";
+import { objectError, buildErrorObject } from "../../utils/utils";
 
-class UserService {
-  public async createUser(data: object): Promise<object> {
-    const user = await User.create(data);
-    console.log("--->", user);
+class UserService{
+  public async createUser(data: object): Promise<objectError | object> {
+    const errorResponse = buildErrorObject("Não foi possível criar o usuário");
+    const createResponse = await User.create(data)
+      .then((result) => result)
+      .catch((error) => errorResponse);
 
-    return user;
+    return createResponse;
+  }
+
+  public async findUser(name: string): Promise<object | objectError> {
+    const errorResponse = buildErrorObject("Não foi possível pegar os dados");
+    const findResponse = await User.find({ name })
+      .then((result) => result)
+      .catch((error) => errorResponse);
+    return findResponse;
+  }
+
+  public async deleteUser(name: string): Promise<object | objectError> {
+    const errorResponse = buildErrorObject("Não foi possível deletar usuário");
+    const deleteResponse = await User.deleteOne({ name })
+      .then((result) => result)
+      .catch((error) => errorResponse);
+    return deleteResponse;
   }
 }
 
