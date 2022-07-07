d449bdd8539f187281268db11960be6c0c95edf8
rebeca.prado <rebeca.2003.prado@gmail.com>
2022-07-07 11:42:40 -0300

    feat: rota GET/POST de produtos

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import  Router  from "express";
 import CategoryController from "./controllers/CategoryController";
+import ProductController from "./controllers/ProductController";
 import UserController from "./controllers/UserController";
 
 const routes = Router()
@@ -9,12 +10,13 @@ routes.post('/v1/user', UserController.create)
 routes.delete('/v1/user', UserController.delete)
 routes.put('/v1/user', UserController.update)
 
-routes.post('/v1/category', CategoryController.create)
 routes.get('/v1/category', CategoryController.find)
+routes.post('/v1/category', CategoryController.create)
 routes.delete('/v1/category', CategoryController.delete)
 routes.put('/v1/category', CategoryController.update)
 
-
-
+routes.post('/v1/product', ProductController.create)
+routes.get('/v1/product', ProductController.findAll)
+routes.get('/v1/product/oneProduct', ProductController.findOne)
 
 export default routes
