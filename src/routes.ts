55f7f44a0c6a1f6c9483a20939e8bfe6277ceaee
rebeca.prado <rebeca.2003.prado@gmail.com>
2022-07-07 10:33:55 -0300

    feat: rotas GET/POST/PUT/DELETE para categorias

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,20 @@
 import  Router  from "express";
+import CategoryController from "./controllers/CategoryController";
 import UserController from "./controllers/UserController";
 
 const routes = Router()
 
-routes.get('/v1/user', UserController.find)
+routes.get('/v1/user/:name', UserController.find)
 routes.post('/v1/user', UserController.create)
 routes.delete('/v1/user', UserController.delete)
+routes.put('/v1/user', UserController.update)
+
+routes.post('/v1/category', CategoryController.create)
+routes.get('/v1/category', CategoryController.find)
+routes.delete('/v1/category', CategoryController.delete)
+routes.put('/v1/category', CategoryController.update)
+
+
+
 
 export default routes
