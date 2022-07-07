967a3f2b3f29cc14aa7c0cb0c0fcb2b621de1cd0
rebeca.prado <rebeca.2003.prado@gmail.com>
2022-07-07 10:37:04 -0300

    feat: interação e validação dos endpoinst de categorias

--- src/controllers/CategoryController.ts
+++ src/controllers/CategoryController.ts
@@ -1,10 +1,68 @@
 import { Response, Request } from "express";
+import CategoryService from "../services/CategoryService";
+import { buildErrorObject } from "../utils/utils";
 
 class CategoryController {
   public async create(req: Request, res: Response): Promise<Response> {
     const { name } = req.body;
 
-    return res.send({ message: "rota ok" });
+    if(!name){
+      return res.status(400).send({message: 'Campos obrigatórios'});
+    }
+
+    const { response, error } = await CategoryService.createCategory({ name });
+
+    if (!error) return res.status(201).send(response);
+
+    const errorResponse = buildErrorObject("Não possível criar a categoria");
+
+    return res.status(400).send(errorResponse);
+  }
+
+  public async find(req: Request, res: Response): Promise<Response> {
+    const { response, error } = await CategoryService.findCategory();
+
+    if (!error) return res.status(200).send(response);
+
+    const errorResponse = buildErrorObject(
+      "Não possível pegar todas as categorias"
+    );
+
+    return res.status(400).send(errorResponse);
+  }
+
+  public async delete(req: Request, res: Response): Promise<Response> {
+    const id = req.headers["id"] as string;
+
+    if (!id) {
+      return res.status(400).send({ message: "é necessario ter id" });
+    }
+
+    const { response, error } = await CategoryService.deleteCategory(id);
+
+    if (!error) return res.status(200).send(response);
+
+    const errorResponse = buildErrorObject("Não possível deletar a categoria");
+
+    return res.status(400).send(errorResponse);
+  }
+  public async update(req: Request, res: Response): Promise<Response> {
+    const id = req.headers["id"] as string;
+
+    if (!id) {
+      return res.status(400).send({ message: "é necessario ter id" });
+    }
+
+    const { name } = req.body;
+    const { response, error } = await CategoryService.updateCategory(id, name);
+
+    if (!error) return res.status(200).send(response);
+
+    const errorResponse = buildErrorObject(
+      "Não possível atualizar essa categoria"
+    );
+
+    return res.status(400).send(errorResponse);
   }
 }
 
