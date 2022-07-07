2c246cc748bea2e4362bf455aab42a501228e45b
rebeca.prado <rebeca.2003.prado@gmail.com>
2022-07-07 10:37:55 -0300

    fix: adição do campo username na model de user

--- src/schemas/User.ts
+++ src/schemas/User.ts
@@ -1,13 +1,21 @@
 import { Schema, model, Document } from "mongoose";
 
+export type UserType = {
+  name: string;
+  username: string;
+  password: string;
+}
+
 interface UserInterface extends Document {
   name: string;
+  username: string;
   password: string;
 }
 
 const UserSchema = new Schema(
   {
-    name: { type: String, required: true, index: { unique: true }},
+    name: { type: String, required: true},
+    username: { type: String, required: true, index: { unique: true }},
     password: { type: String, required: true },
   },
   {
