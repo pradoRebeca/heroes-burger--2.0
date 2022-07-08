6c72d486f4488bc36f6e3ebcad2e001c618b55d8
rebeca.prado <rebeca.2003.prado@gmail.com>
2022-07-08 11:43:33 -0300

    fix: correção da escrita no retorno das funções

--- src/services/CategoryService.ts
+++ src/services/CategoryService.ts
@@ -1,46 +1,45 @@
 import Category from "../schemas/Category";
-import { objectResponse} from "../utils/utils";
+import { objectResponse } from "../utils/utils";
 
 class CategoryService {
   public async createCategory(obj: object): Promise<objectResponse> {
-
     const responseCreate = await Category.create(obj)
-      .then((result) => ({response: result}))
-      .catch((error) => ({error: true, reponse: error}));
+      .then((result) => ({ response: result }))
+      .catch((error) => ({ error: true, response: error }));
 
     return responseCreate;
   }
 
   public async findCategory(): Promise<objectResponse> {
-
     const responseFind = await Category.find()
-      .then((result) => ({response: result}))
-      .catch((error) => ({error: true, reponse: error}));
+      .then((result) => ({ response: result }))
+      .catch((error) => ({ error: true, response: error }));
 
     return responseFind;
   }
 
-
   public async deleteCategory(id: string): Promise<objectResponse> {
-    const deleteToId = {_id: id}
+    const deleteToId = { _id: id };
     const responseDelete = await Category.deleteOne(deleteToId)
-      .then((result) => ({response: result}))
-      .catch((error) => ({error: true, reponse: error}));
+      .then((result) => ({ response: result }))
+      .catch((error) => ({ error: true, response: error }));
 
     return responseDelete;
   }
 
-  public async updateCategory(id: string, name: string): Promise<objectResponse> {
-    const filter = {_id: id}
-    const update = {name}
+  public async updateCategory(
+    id: string,
+    name: string
+  ): Promise<objectResponse> {
+    const filter = { _id: id };
+    const update = { name };
 
-    const responseDelete = await Category.updateOne(filter,update)
-      .then((result) => ({response: result}))
-      .catch((error) => ({error: true, reponse: error}));
+    const responseDelete = await Category.updateOne(filter, update)
+      .then((result) => ({ response: result }))
+      .catch((error) => ({ error: true, response: error }));
 
     return responseDelete;
   }
-
 }
 
 export default new CategoryService();
