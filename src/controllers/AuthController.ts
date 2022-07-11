30a7861f5c8e0cdab599699a2db3a95f6ae4e90a
rebeca.prado <rebeca.2003.prado@gmail.com>
2022-07-11 10:33:31 -0300

    feat: autenticação com json web token

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,22 +1,32 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import { UserType } from "../schemas/User";
 import UserService from "../services/UserService";
-import { objectResponse } from "../utils/utils";
+// import { objectResponse } from "../utils/utils"
+
+interface tokenPayload {
+  id: string;
+  iat: number;
+  exp: number;
+}
 
 class AuthController {
-  public async jsonWebToken(username: string): Promise<objectResponse> {
-    const SECRET = "private";
 
+
+  public async verifyJWT(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void> {
+    const token = req.headers["x-access-token"] as string;
     try {
-      const token = "abc";
-      jwt.sign({ username: username }, SECRET, {
-        expiresIn: 200,
-      });
-
-      return { response: token };
+      const {id} = jwt.verify(token, "private") as tokenPayload;
+      // console.log("id usuario =>", data);
+      req.userId = id;
+      return next();
     } catch (error) {
-      return { error: true, response: error };
+      console.log(error);
+      return res.status(401).send({ message: "Usuário não autenticado" });
     }
   }
 
@@ -32,26 +42,30 @@ class AuthController {
     );
 
     if (error) {
-      console.log();
       return res
         .status(400)
         .send({ message: "não foi possivel fazer a autenticação" });
     }
 
-    // const token = await this.jsonWebToken(username);
-    // console.log(token);
-    // if (token.error)
-    //   return res
-    //     .status(400)
-    //     .send({ message: "não foi possivel fazer o token" });
-
     const lengthUser = response as Array<object>;
-
     if (lengthUser.length == 0) {
       return res.status(404).send({ message: "User not found" });
     }
 
-    return res.status(200);
+    try {
+      const { _id } = lengthUser[0] as UserType;
+
+      const token = jwt.sign({ id: _id }, "private", {
+        expiresIn: 200,
+      });
+
+      return res.status(200).send({ token });
+    } catch (error) {
+      console.log("catch =>", error);
+      return res
+        .status(400)
+        .send({ message: "Não foi possivel gerar o token" });
+    }
   }
 }
 
